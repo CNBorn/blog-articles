19683574812f5728296d42eb6df0f8e0e0cd65b1
CNBorn <CNBorn@hotmail.com>
2014-09-05 11:20:13 +0800

    change rss config

--- pelican.conf.py
+++ pelican.conf.py
@@ -2,7 +2,8 @@
 AUTHOR = u'Tyler Xing'
 SITENAME = u"Tyler Xing"
 TAGLINE = u"Dharma's truth"
-SITEURL = 'http://cnborn.net/blog/'
+# SITEURL = 'http://cnborn.net/blog/'
+SITEURL = 'http://localhost:8000'
 
 ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
@@ -13,8 +14,17 @@ PAGE_SAVE_AS = 'pages/{slug}/index.html'
 CATEGORY_URL = 'category/{name}/'
 CATEGORY_SAVE_AS = 'category/{name}/index.html'
 AUTHOR_SAVE_AS = ''
-ARTICLE_LANG_SAVE_AS = ARTICLE_SAVE_AS
 DRAFT_SAVE_AS = ''
+ARTICLE_LANG_URL = ARTICLE_URL
+ARTICLE_LANG_SAVE_AS = ARTICLE_SAVE_AS
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = ''
+CATEGORY_FEED_RSS = ''
+AUTHOR_FEED_ATOM = ''
+AUTHOR_FEED_RSS = ''
+TRANSLATION_FEED_ATOM = ''
+TRANSLATION_FEED_RSS = ''
 
 GITHUB_URL = 'http://github.com/CNBorn/'
 DISQUS_SITENAME = "cnborn"
@@ -22,12 +32,9 @@ PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
 LOCALE = ""
 DEFAULT_DATE_FORMAT = "%Y.%m.%d"
-DEFAULT_PAGINATION = 1
+DEFAULT_PAGINATION = 1000
 DEFAULT_ORPHANS = 0
 GOOGLE_ANALYTICS = "UA-329713-5"
-
-FEED_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 PLUGIN_PATHS = ['/Users/benxing/Projects/',]
 PLUGINS = ['pelican-langcategory']
@@ -42,17 +49,8 @@ MENUITEMS = (
     ('CN', '/blog/lang/cn/'),
 )
 
-LINKS = (('OpenParty', 'http://www.beijing-open-party.org'),
-        )
-
-SOCIAL = (('Twitter', 'http://twitter.com/CNBorn'),
-          ('Douban', 'http://www.douban.com/people/CNBorn/'),
-          ('github', 'http://github.com/CNBorn'),
-         )
-
 LINKS = ()
 SOCIAL = ()
-
 
 # global metadata to all the contents
 DEFAULT_METADATA = (('CNBorn', 'cnborn'),)
